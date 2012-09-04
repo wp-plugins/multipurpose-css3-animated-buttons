4dee467e88ae716325000f87d7d8af6d5bb94764
erk i <erk i@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-04 11:27:11 +0000

    settings link in plugins page - function name fix
    git-svn-id: https://plugins.svn.wordpress.org/multipurpose-css3-animated-buttons/trunk@594379 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mab.php
+++ mab.php
@@ -31,6 +31,17 @@
 */
 
 /**
+* ADMIN NOTICE
+*
+*/
+add_action( 'admin_notices', 'mab_admin_notice' );
+function mab_admin_notice(){
+     global $mab_first_settings;
+     if ( $mab_first_settings == '1' )
+          echo '<div><p>You should configure "Multipurpose CSS3 Animated Buttons" ' . '<a href="plugins.php?page=mab_plugin_settings">'.__("Settings", "mab").'</a></p></div>';
+}
+
+/**
 * Add Settings link to plugins
 *
 */
